d0b1812e73bff9946a017516be8c42eae3bb4535
onli <onli@paskuda.biz>
2025-11-13 13:42:52 +0100

    Fill in logic to create task

--- include/database.php
+++ include/database.php
@@ -15,14 +15,27 @@ class Database {
 	}
 
 	function setupDB() {
-	
-	
 		$query = 'CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY AUTOINCREMENT, name TEXT, role TEXT, email TEXT)'; 
 		$stmt = $this->db->prepare($query);
 		$stmt->execute();
 		
 		$query = 'CREATE TABLE IF NOT EXISTS tasks (id integer PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, creation_date NUMERIC, target_date NUMERIC, category TEXT, priority INTEGER, owner INTEGER)'; 
 		$stmt = $this->db->prepare($query);
+		$stmt->execute();
+	}
+	
+	function createTask($title, $description, $targetDate, $category, $priority = 0) {
+		$targetDateEpoch = strtotime($targetDate);
+		
+		$query = 'INSERT INTO tasks(title, description, creation_date, target_date, category, priority, owner) VALUES(?, ?, ?, ?, ?, ?, 0)';
+		$stmt = $this->db->prepare($query);
+		$stmt->bindParam(1, $title); 
+		$stmt->bindParam(2, $description); 
+		$stmt->bindParam(3, time()); 
+		$stmt->bindParam(4, $targetDateEpoch); 
+		$stmt->bindParam(5, $category); 
+		$stmt->bindParam(6, $priority); 
+		// $stmt->bindParam(7, 0);  // # TODO: Link to user currently logged in
 		$stmt->execute();
 	}
 }
--- include/router.php
+++ include/router.php
@@ -10,13 +10,17 @@ class Router {
 		# TODO: Insert templating layer here	
 		switch($_REQUEST['page']) {
 			case 'create':
+			
+				$timestamp = time() + (60 * 60 * 24 * 7);
+				$date = date( "Y-m-d", $timestamp );
+
 				echo '<form action="?page=do_create" method="POST">
 					<label for="title">Titel</label>
 					<input name="title" />
 					<label for="description">Beschreibung</label>
 					<textarea name="descrition"></textarea>
 					<label for="target_date">Zieldatum</label>
-					<input name="target_date" type="date" />
+					<input name="target_date" type="date" value="' . $date . '" />
 					<label for="category">Kategorie</label>
 					<input name="category" />
 					<label for="priority">Wichtigkeit</label>
@@ -25,6 +29,13 @@ class Router {
 				</form>
 				';
 				break;
+			
+			case 'do_create':
+				$db = new Database();
+				$db->createTask($_POST['title'], $_POST['description'], $_POST['target_date'], $_POST['category'], $_POST['priority']);  
+				
+				header("Location: /tasks");
+				die();
 			
 			default:
 				echo '<a href="?page=create">New Tasks</a>';
