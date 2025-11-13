5c58e33fa553ef5ad87013624f5daa74bff4f9ea
onli <onli@paskuda.biz>
2025-11-13 12:00:29 +0100

    init form to create new tasks No styling so far

--- include/router.php
+++ include/router.php
@@ -6,13 +6,28 @@ namespace onli\tasks;
 class Router {
 	
 	
-	function servePage() {
-		print_r($_REQUEST);
-		
+	function servePage() {	
+		# TODO: Insert templating layer here	
 		switch($_REQUEST['page']) {
+			case 'create':
+				echo '<form action="?page=do_create" method="POST">
+					<label for="title">Titel</label>
+					<input name="title" />
+					<label for="description">Beschreibung</label>
+					<textarea name="descrition"></textarea>
+					<label for="target_date">Zieldatum</label>
+					<input name="target_date" type="date" />
+					<label for="category">Kategorie</label>
+					<input name="category" />
+					<label for="priority">Wichtigkeit</label>
+					<input name="priority" type="number" min=0 max=5 />
+					<button>Speichern</button>
+				</form>
+				';
+				break;
 			
 			default:
-				echo 'tasks';
+				echo '<a href="?page=create">New Tasks</a>';
 				break;
 		}
 		
